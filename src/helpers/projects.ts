245857b3bab828faa7142959426488c0e3a27492
matta-ale <matta.ale@gmail.com>
2023-12-28 09:58:59 -0300

    Agregada info de proyectos

--- src/helpers/projects.ts
+++ src/helpers/projects.ts
@@ -31,10 +31,18 @@ export const project = [
   },
   {
     icon: [faPython],
-    name: 'Videogames',
+    name: '3barhl',
     description:
-      'Individual Project for soyHenry bootcamp. It fetches videogames from an API and features a database to create and save new ones',
-    projectLink: 'http://52.15.170.33/',
-    githubLink: 'https://github.com/matta-ale/PI-VIDEOGAMES',
+      'Trading bot for 3barhl trading strategy. This bot trades different cryptocurrencies through Binance exchange API. It operates 24/7 and logs what it is doing and results to a telegram chat.',
+    projectLink: '#',
+    githubLink: 'https://github.com/matta-ale/3barhl',
+  },
+  {
+    icon: [faPython],
+    name: 'Momentum',
+    description:
+      'Trading bot for Momentum trading strategy. This bot trades different cryptocurrencies through Binance exchange API. It operates 24/7 and logs what it is doing and results to a telegram chat.',
+    projectLink: '#',
+    githubLink: 'https://github.com/matta-ale/momentum',
   },
 ];
