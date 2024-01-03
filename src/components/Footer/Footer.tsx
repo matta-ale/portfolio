4e9d6300e996d387103dd8cf245b9b9a2906ef48
matta-ale <matta.ale@gmail.com>
2024-01-03 14:40:41 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../helpers/pathRouters';
 
 const Footer = () => {
   return (
     <div className='flex justify-center'>
-      <div className='w-[100%] max-w-[1200px] h-24 bg-white z-49 border-t-2 grid grid-cols-1 md:grid-cols-2 '>
+      <div className='w-[100%] max-w-[1200px] h-24 bg-white z-49 border-t-2 flex justify-center items-center grid grid-cols-1 md:grid-cols-2 '>
         <div className='w-3/5 ml-12'>
           <ul className='flex justify-around font-sans text-xs text-gray-500 font-semibold'>
             <li className='footer-nav-link-item'>
